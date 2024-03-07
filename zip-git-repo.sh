98171b42470bf5814e8d5498f24e81a7eb87a4dc
van-joseph <van.cabuga@awsys-i.com>
2024-03-07 16:18:43 +0800

    Cleaner Repository

---
+++ zip-git-repo.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+#ask for information, use exec < dev/tty to allow prompt in post-commit
+#dont forget to ensure the EOL aligns with unix specification: dos2unix <filename>
+read -p "Enter Comment:" commentVar
+
+dateVar=$(date +%Y%m%d)
+fullName="${commentVar}${dateVar}"
+
+
+zip -r ./$fullName.zip ./ -x ./$fullName.zip
